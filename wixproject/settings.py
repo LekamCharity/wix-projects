db6e9f00a24aea738dba956c957c8c4b1b3afb34
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-12-01 15:44:05 +0300

    new changes

--- wixproject/settings.py
+++ wixproject/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 import django_heroku
 from decouple import config,Csv
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -94,6 +95,8 @@ WSGI_APPLICATION = 'wixproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
+
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -102,7 +105,7 @@ if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('awwards'),
+           'NAME': config('DB_NAME'),
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
            'HOST': config('DB_HOST'),
@@ -122,6 +125,9 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+
+
 
 
 # Password validation
@@ -178,6 +184,7 @@ LOGIN_URL ='login'
 STAR_RATINGS_RERATE = False
 STAR_RATINGS_RANGE = 5
 STAR_RATINGS_ANONYMOUS = False
+
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
